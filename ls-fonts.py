fda1be5fa0e0586a8dbb14328772395b871c9a95
Koji Ishii <kojii@chromium.org>
2024-04-05 22:34:11 +0900

    ls-fonts: Support `[name]` syntax

--- ls-fonts.py
+++ ls-fonts.py
@@ -4,6 +4,7 @@
 
 import argparse
 from pathlib import Path
+import re
 
 def split_with_default(input: str, separator: str, max: int):
     result = input.split(separator, max)
@@ -27,20 +28,29 @@ def split_style_locale(name: str):
     pitch, locale = split_prefix(name, 'Mono')
     return (style, pitch, locale)
 
-def to_size_tuple(file: Path):
-    name, suffix = split_with_default(file.name, '.', 1)
-    name, weight = split_with_default(name, '-', 1)
-    style, pitch, locale = split_style_locale(name)
-    return (
-        file.name,
-        file.stat().st_size,
-        style,
-        pitch,
-        locale,
-        weight,
-        suffix,
-        # file.parent,
-    )
+class FontStat(object):
+
+    def __init__(self, path: Path) -> None:
+        self.path = path
+        name, self.suffix = split_with_default(path.name, '.', 1)
+        name, self.weight = split_with_default(name, '-', 1)
+        variables = re.search(r'\[[^]]+\]$', name)
+        if variables:
+            self.weight = variables.group(0)
+            name = name[0:-len(self.weight)]
+        self.style, self.pitch, self.locale = split_style_locale(name)
+
+    def to_tuple(self):
+        return (
+            self.path.name,
+            self.path.stat().st_size,
+            self.style,
+            self.pitch,
+            self.locale,
+            self.weight,
+            self.suffix,
+            # self.path.parent,
+        )
 
 def main():
     parser = argparse.ArgumentParser()
@@ -48,8 +58,8 @@ def main():
     args = parser.parse_args()
     for arg in args.dir:
         dir = Path(arg)
-        for file in dir.glob('./**/*.[ot]t[cf]'):
-            tuple = to_size_tuple(file)
+        for path in dir.glob('./**/*.[ot]t[cf]'):
+            tuple = FontStat(path).to_tuple()
             tuple = (('' if i is None else i) for i in tuple)
             tuple = (str(i) for i in tuple)
             print('\t'.join(tuple))
